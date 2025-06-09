58b2963e156d77f5a014921cea4fd68be8c2e5fe
Pallavi <pallaviy289@gmail.com>
2025-06-09 15:58:48 +0530

    Add files via upload

---
+++ InDe.java
@@ -0,0 +1,15 @@
+class InDe
+{
+	public static void main(String args[])
+	 {
+    		int a = 5, b = 5;
+
+    		int x = ++a + ++a;  // Pre-increment
+    		int y = a++  + a++;  // Post-increment
+		System.out.println(x);
+		System.out.println(y);
+	
+	
+	}
+
+}
